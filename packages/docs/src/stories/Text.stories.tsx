6eac4c1ccd01f5dbace4de479df9892d879aea02
Ednaldo Cordeiro <ednaldocordeiro2017@gmail.com>
2023-10-21 17:41:16 -0300

    Text

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import { Text, TextProps } from '@ignite-ui/react'
+import { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Typograpohy/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa exercitationem sunt debitis doloribus, perspiciatis necessitatibus a id, corporis et ipsam error, commodi explicabo aliquam architecto ea laborum? Laboriosam, neque facilis!',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong Text',
+    as: 'strong',
+  },
+}
